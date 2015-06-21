70207c1d6703972b61e154a3fa2fefcf2765ef8a
Sean Wolcott <mcvoid@mcvoid.org>
2015-06-21 10:11:51 -0400

    ran go fmt on the code

--- src/go-http/go-http.go
+++ src/go-http/go-http.go
@@ -1,112 +1,107 @@
 package main
 
 import (
-	"io"
-	"net/http"
 	"flag"
-	"log"
-	"io/ioutil"
 	"fmt"
+	"io"
+	"io/ioutil"
+	"log"
+	"net/http"
 	"os"
 )
 
 var defaultPath string
 var baseURL string
 
-
 func Handler(w http.ResponseWriter, req *http.Request) {
-	
+
 	filename := defaultPath + req.URL.Path[1:]
-	if last := len(filename) - 1; last >= 0 && filename[last] == '/' && len(filename)!=1 {
-        filename = filename[:last]
-    }
-	
-	if(filename==""){
+	if last := len(filename) - 1; last >= 0 && filename[last] == '/' && len(filename) != 1 {
+		filename = filename[:last]
+	}
+
+	if filename == "" {
 		// fmt.Println("request is empty ")
 		filename = "./"
 	}
 
-	
 	// If file Exists
 	if file, err := os.Stat(filename); os.IsNotExist(err) {
-    	
-    	_,err = io.WriteString(w, "404 Not Found")
 
-    	return
-	
-	} else{
+		_, err = io.WriteString(w, "404 Not Found")
+
+		return
+
+	} else {
 
 		if file.IsDir() {
-			
+
 			slashCheck := ""
 
 			files, _ := ioutil.ReadDir(filename)
-			if filename != "./"{
-				if filename[len(filename)-1] !='/'{
+			if filename != "./" {
+				if filename[len(filename)-1] != '/' {
 					slashCheck = "/"
 				}
 			}
 
-			responseString := "<html><body> <h3> Directory Listing for "+req.URL.Path[1:] +"/ </h3> <br/> <hr> "
-	    	for _, f := range files {
-	            if(f.Name()[0]!= '.') {
-	            	if(f.IsDir()){
-			            newLink := "<a href=\"" + baseURL + req.URL.Path[0:] + slashCheck + f.Name() + "\">" + f.Name() + "/" + "</a><br/>"
-			            responseString = responseString + newLink
-	            	} else {
-			            newLink := "<a href=\"" + baseURL + req.URL.Path[0:] + slashCheck + f.Name() + "\">" + f.Name() + "</a><br/>"
-			            responseString = responseString + newLink
-	            	}
-	            }
-	    	}
-	    	responseString = responseString + "</body></html>"
-	    	_,err = io.WriteString(w, responseString)
-	    	if err != nil {
-		        // panic(err)
+			responseString := "<html><body> <h3> Directory Listing for " + req.URL.Path[1:] + "/ </h3> <br/> <hr> "
+			for _, f := range files {
+				if f.Name()[0] != '.' {
+					if f.IsDir() {
+						newLink := "<a href=\"" + baseURL + req.URL.Path[0:] + slashCheck + f.Name() + "\">" + f.Name() + "/" + "</a><br/>"
+						responseString = responseString + newLink
+					} else {
+						newLink := "<a href=\"" + baseURL + req.URL.Path[0:] + slashCheck + f.Name() + "\">" + f.Name() + "</a><br/>"
+						responseString = responseString + newLink
+					}
+				}
+			}
+			responseString = responseString + "</body></html>"
+			_, err = io.WriteString(w, responseString)
+			if err != nil {
+				// panic(err)
 				http.Redirect(w, req, "", http.StatusInternalServerError)
-		    }
-		}else{
-			
-			
+			}
+		} else {
+
 			b, err := ioutil.ReadFile(filename)
-		    if err != nil {
+			if err != nil {
 				http.Redirect(w, req, "", http.StatusInternalServerError)
-		    	return
-		    }else{
-			    str := string(b)
-				_,err = io.WriteString(w, str)
-			    if err != nil {
-			        // panic(err)
+				return
+			} else {
+				str := string(b)
+				_, err = io.WriteString(w, str)
+				if err != nil {
+					// panic(err)
 					http.Redirect(w, req, "", http.StatusInternalServerError)
-			    }
-		    }
+				}
+			}
 		}
-	
 
 	}
 }
 
 func main() {
 
-	
-	defaultPortPtr := flag.String("p","","Port Number")
-	defaultPathPtr := flag.String("d","","Root Directory")
+	defaultPortPtr := flag.String("p", "", "Port Number")
+	defaultPathPtr := flag.String("d", "", "Root Directory")
 	flag.Parse()
 
-	if *defaultPathPtr !=""{
+	if *defaultPathPtr != "" {
 		defaultPath = "./" + *defaultPathPtr + "/"
 	} else {
 		defaultPath = ""
 	}
 
 	portNum := "8080"
-	if *defaultPortPtr !=""{
+	if *defaultPortPtr != "" {
 		portNum = *defaultPortPtr
 	}
 
-	baseURL = "http://localhost:" + portNum;
+	baseURL = "http://localhost:" + portNum
 
-	fmt.Println("Serving on ",baseURL, " subdirectory ", defaultPath)
+	fmt.Println("Serving on ", baseURL, " subdirectory ", defaultPath)
 
 	http.HandleFunc("/", Handler)
 	err := http.ListenAndServe(":"+portNum, nil)
