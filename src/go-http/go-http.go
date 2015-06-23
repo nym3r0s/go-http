40a532efab1d2e3ef62c788075209867e225b9a6
Gokul Srinivas <gokusrinivas@gmail.com>
2015-06-23 16:35:33 +0530

    Added Logger Added the logger that logs the request on the terminal. Logs the URL, date, time, HTTP Method, HTTP response code, Request URL

--- src/go-http/go-http.go
+++ src/go-http/go-http.go
@@ -9,12 +9,19 @@ import (
 	"net/http"
 	"os"
 	"path"
+	"strconv"
 	"strings"
+	"time"
 )
 
 var defaultPath string
 
 var baseURL string
+
+func Logger(req *http.Request, statusCode int) {
+	const layout = "[ 2/Jan/2006 15:04:05 ]"
+	fmt.Println(baseURL + " --- " + time.Now().Format(layout) + " " + req.Method + "  " + strconv.Itoa(statusCode) + "  " + req.URL.Path)
+}
 
 func Handler(w http.ResponseWriter, req *http.Request) {
 
@@ -23,8 +30,8 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 		filename = filename[:last]
 	}
 
+	// Empty request (Root)
 	if filename == "" {
-		// fmt.Println("request is empty ")
 		filename = "./"
 	}
 
@@ -32,9 +39,8 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 
 	// 404 if file doesn't exist
 	if os.IsNotExist(err) {
-
 		_, err = io.WriteString(w, "404 Not Found")
-
+		Logger(req, http.StatusNotFound)
 		return
 	}
 
@@ -43,23 +49,32 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 
 		slashCheck := ""
 
-		files, _ := ioutil.ReadDir(filename)
+		files, err := ioutil.ReadDir(filename)
+		// Catch the Error in reading from directory
+		if err != nil {
+			http.Redirect(w, req, "", http.StatusInternalServerError)
+			Logger(req, http.StatusInternalServerError)
+		}
+		// Checking for Root Directory
 		if filename != "./" {
 			if filename[len(filename)-1] != '/' {
 				slashCheck = "/"
 			}
 		}
 
-		responseString := "<html><body> <h3> Directory Listing for " + req.URL.Path[1:] + "/ </h3> <br/> <hr> "
+		responseString := "<html><body> <h3> Directory Listing for " + req.URL.Path[1:] + "/ </h3> <br/> <hr> <ul>"
 		for _, f := range files {
 			if f.Name()[0] != '.' {
 				if f.IsDir() {
-					responseString += "<a href=\"" + baseURL + req.URL.Path[0:] + slashCheck + f.Name() + "\">" + f.Name() + "/" + "</a><br/>"
+					responseString += "<li><a href=\"" + baseURL + req.URL.Path[0:] + slashCheck + f.Name() + "\">" + f.Name() + "/" + "</a></li>"
 				} else {
-					responseString += "<a href=\"" + baseURL + req.URL.Path[0:] + slashCheck + f.Name() + "\">" + f.Name() + "</a><br/>"
+					responseString += "<li><a href=\"" + baseURL + req.URL.Path[0:] + slashCheck + f.Name() + "\">" + f.Name() + "</a></li>"
 				}
 			}
 		}
+
+		//Ending the list
+		responseString += "</ul><br/><hr/>"
 
 		p := req.URL.Path
 
@@ -84,6 +99,9 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 		if err != nil {
 			// panic(err)
 			http.Redirect(w, req, "", http.StatusInternalServerError)
+			Logger(req, http.StatusInternalServerError)
+		} else {
+			Logger(req, http.StatusOK)
 		}
 
 		return
@@ -94,6 +112,7 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 	b, err := ioutil.ReadFile(filename)
 	if err != nil {
 		http.Redirect(w, req, "", http.StatusInternalServerError)
+		Logger(req, http.StatusInternalServerError)
 		return
 	}
 
@@ -102,6 +121,8 @@ func Handler(w http.ResponseWriter, req *http.Request) {
 	if err != nil {
 		// panic(err)
 		http.Redirect(w, req, "", http.StatusInternalServerError)
+	} else {
+		Logger(req, http.StatusOK)
 	}
 
 }
@@ -112,15 +133,21 @@ func main() {
 	defaultPathPtr := flag.String("d", "", "Root Directory")
 	flag.Parse()
 
+	portNum := "8080"
+
+	// Handling the command line flags
+
+	// Directory
 	if *defaultPathPtr != "" {
 		defaultPath = "./" + *defaultPathPtr + "/"
 	} else {
 		defaultPath = ""
 	}
-
-	portNum := "8080"
+	// Port Number
 	if *defaultPortPtr != "" {
 		portNum = *defaultPortPtr
+	} else {
+		portNum = "8080"
 	}
 
 	baseURL = "http://localhost:" + portNum
